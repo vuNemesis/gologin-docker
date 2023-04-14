6f427aa20daf75c1c24cde099d0299e425179077
vuNemesis <vunemesis@gmail.com>
2023-04-15 00:30:52 +0300

    fix orbita wsUrl

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ set -a
 source <(cat .env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
 set +a
 
-docker run -e SCREEN_WIDTH=$SCREEN_WIDTH -e SCREEN_HEIGHT=$SCREEN_HEIGHT -e PROFILE_ID=$PROFILE_ID -e TOKEN=$TOKEN --rm -ti -p 5901:5901 -p 3000:3000 --name orbita-browser orbita-docker:latest 
+docker run -e SCREEN_WIDTH=$SCREEN_WIDTH -e SCREEN_HEIGHT=$SCREEN_HEIGHT -e PROFILE_ID=$PROFILE_ID -e TOKEN=$TOKEN --rm -ti -p 5901:5901 -p 3000:3000 --name orbita-browser orbita-docker:latest
--- test.js
+++ test.js
@@ -1,13 +1,28 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer-core')
 
-(async () =>{
+const getOrbitaWsUrl = async () => {
+  const orbitaPort = 3000
+  const res = await fetch(`http://127.0.0.1:${orbitaPort}/json/version`)
+  let wsUrl = (await res.json()).webSocketDebuggerUrl
+
+  return wsUrl.replace('/127.0.0.1/', `/127.0.0.1:${orbitaPort}/`)
+}
+
+;(async () => {
+  try {
+    const orbitaWsUrl = await getOrbitaWsUrl()
+
     const browser = await puppeteer.connect({
-        browserURL: 'http://127.0.0.1:3000', 
-        ignoreHTTPSErrors: true,
-    });
+      //   browserURL: "http://127.0.0.1:3000",
+      browserWSEndpoint: orbitaWsUrl,
+      ignoreHTTPSErrors: true,
+    })
 
-    let pages = await browser.pages();
-    let page  = pages[0];
-    await page.goto('https://myip.link/mini');   
-    console.log(await page.content());
-})();
+    let pages = await browser.pages()
+    let page = pages[0]
+    await page.goto('https://myip.link/mini')
+    console.log(await page.content())
+  } catch (e) {
+    console.error(e)
+  }
+})()
